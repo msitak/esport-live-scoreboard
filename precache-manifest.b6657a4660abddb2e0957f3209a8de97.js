1812575165762e436b434479c8be413cdcef9a64
Michał Sitak <michalsitak@macbook-air-micha.home>
2024-07-01 13:11:38 +0200

    Updates

---
+++ precache-manifest.b6657a4660abddb2e0957f3209a8de97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935e898b7cefb64de21a4c2fe5050d71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3cf56ad7b455467c0c8",
+    "url": "/static/js/2.47723302.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.47723302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f9315d26c0618393ba",
+    "url": "/static/js/main.b22d1517.chunk.js"
+  },
+  {
+    "revision": "b8f13d1d0c9bbcd87f73",
+    "url": "/static/js/runtime-main.d0edaa2b.js"
+  }
+]);
